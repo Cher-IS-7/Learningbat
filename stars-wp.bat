7775dd18c5458e6402616acb78660c23fb18e0b5
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-05 12:35:26 +0000

    Update stars-wp.bat

--- stars-wp.bat
+++ stars-wp.bat
@@ -1,12 +1,17 @@
 @echo off
-Set "ImageURL=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp6734135.jpg&f=1&nofb=1&ipt=b6c95065ea4e03aae50baefe79946e29a80c7de0cfbb4a261ccfc78bc961aa12&ipo=images"
 
-:: Using the user's Temp directory
-Set "ImagePath=%TEMP%\Wallpaper.jpg"
+set "image_url=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp6734135.jpg&f=1&nofb=1&ipt=b6c95065ea4e03aae50baefe79946e29a80c7de0cfbb4a261ccfc78bc961aa12&ipo=images"
+set "output_file=%TEMP%\wallpaper.png"
 
-PowerShell -Command "(New-Object System.Net.WebClient).DownloadFile('%ImageURL%', '%ImagePath%')"
+REM Download the image using certutil
+certutil -urlcache -split -f "%image_url%" "%output_file%"
 
-REG ADD "HKCU\Control Panel\Desktop" /V Wallpaper /T REG_SZ /F /D "%ImagePath%"
-REG ADD "HKCU\Control Panel\Desktop" /V WallpaperStyle /T REG_SZ /F /D 0
-REG ADD "HKCU\Control Panel\Desktop" /V TileWallpaper /T REG_SZ /F /D 2
-RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters ,1 ,True
+REM Set the downloaded image as desktop wallpaper
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%output_file%" /f
+reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+reg add "HKCU\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d 0 /f
+
+REM Refresh the desktop to apply changes
+rundll32.exe user32.dll,UpdatePerUserSystemParameters
+
+echo Wallpaper set successfully.
