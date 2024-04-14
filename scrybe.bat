3891a8b1fb00956b660cea8acb18cb483ce059b6
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-14 21:42:25 +0400

    Update scrybe.bat

--- scrybe.bat
+++ scrybe.bat
@@ -8,6 +8,9 @@ set DESKTOP=%USERPROFILE%\Desktop
 rem Generate systeminfo.txt on the desktop
 systeminfo > "%DESKTOP%\%FILENAME1%"
 
+rem Run ping to the default gateway and append the output to a text file
+ping -n 1 %DESKTOP% >> "%DESKTOP%\%FILENAME%"
+
 rem Generate ipInfo.txt on the desktop
 ipconfig > "%DESKTOP%\%FILENAME2%"
 
