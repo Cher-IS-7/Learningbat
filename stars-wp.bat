c935deaa9ff939d28fd91bb82c119acb80f86409
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-14 21:14:08 +0400

    Update stars-wp.bat

--- stars-wp.bat
+++ stars-wp.bat
@@ -1,17 +1,15 @@
 @echo off
 
-set "image_url=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp6734135.jpg&f=1&nofb=1&ipt=b6c95065ea4e03aae50baefe79946e29a80c7de0cfbb4a261ccfc78bc961aa12&ipo=images"
-set "output_file=%TEMP%\wallpaper.png"
+setlocal
 
-REM Download the image using certutil
-certutil -urlcache -split -f "%image_url%" "%output_file%"
+rem Step 1: Download the image from the URL
+curl -o wallpaper.jpg <https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F9%2F98%2FMilky_Way_Galaxy_shimmering_over_Nanga_Parbat%2C_Pakistan.jpg&f=1&nofb=1&ipt=885b3d7fdb0db1df779539afd0b53d946ad5cde9c200c706a90c787139b74688&ipo=images>
 
-REM Set the downloaded image as desktop wallpaper
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%output_file%" /f
-reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
-reg add "HKCU\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d 0 /f
+rem Step 2: Set the downloaded image as the wallpaper
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%CD%\wallpaper.jpg" /f
 
-REM Refresh the desktop to apply changes
-rundll32.exe user32.dll,UpdatePerUserSystemParameters
+rem Step 3: Update the wallpaper setting
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
 
-echo Wallpaper set successfully.
+endlocal
+
