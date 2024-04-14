a0eba0c9d93d361d3b333e1e0b09b769a20d70c8
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-14 21:35:56 +0400

    Update scrybe.bat

--- scrybe.bat
+++ scrybe.bat
@@ -1,15 +1,15 @@
 @echo off
-set FILENAME=systeminfo.txt
-set FILENAME=ipInfo.txt
+set FILENAME1=systeminfo.txt
+set FILENAME2=ipInfo.txt
 
 rem Set the path to the desktop folder
 set DESKTOP=%USERPROFILE%\Desktop
 
 rem Generate systeminfo.txt on the desktop
-systeminfo > "%DESKTOP%\%FILENAME%"
+systeminfo > "%DESKTOP%\%FILENAME1%"
 
 rem Generate ipInfo.txt on the desktop
-ipconfig > "%DESKTOP%\%FILENAME%"
+ipconfig > "%DESKTOP%\%FILENAME2%"
 
 pause
 
